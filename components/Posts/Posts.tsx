e95df4574c25be00d9c8dde774234649b5dc649e
kalysh_maxym <503292@ukr.net>
2020-06-12 19:30:42 +0300

    FIX _ styled-components

--- components/Posts/Posts.tsx
+++ components/Posts/Posts.tsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import * as API from '../../services/api';
 
+type PostsType = {
+  id: number | null;
+  title: string | null;
+  body: string | null;
+};
+
 const Posts = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     API.getPosts()
       .then(response => {
-        const posts = response.data;
-        console.log(posts, 'posts');
+        const posts: Array<PostsType> = response.data as Array<PostsType>;
+        // console.log(posts, 'posts');
         setPosts(posts);
       })
       .catch(error => {
@@ -19,9 +25,14 @@ const Posts = () => {
   return (
     <>
       Posts
-      {posts.map(el => {
-        return <p>{el.title}</p>;
-      })}
+      <ul>
+        {posts.map(el => (
+          <li key={el.id}>
+            <p>{el.title}</p>
+            <p>{el.body}</p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
