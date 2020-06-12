91eb23fab4c4e48ce654293c71651d4a5c945b35
kalysh_maxym <503292@ukr.net>
2020-06-12 17:10:15 +0300

    FIXING bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from '../src/redux/store';
-import App from '../src/components/App';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../src/redux/store";
+import App from "../src/components/App";
 
 const IndexPage = () => (
   <>
