d84ecb49947e524ce0864349a25dcb61a80fee70
kalysh_maxym <503292@ukr.net>
2020-06-12 17:46:23 +0300

    ADD ._ styled-components

--- components/App.tsx
+++ components/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Posts from './Posts/Posts';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const App = () => (
-    <>Apppprrrrrr</>
+  <Container>
+    <h2>Apppprrrrrr</h2>
+    <Posts />
+  </Container>
 );
 
 export default App;
