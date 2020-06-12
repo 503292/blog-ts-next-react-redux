b0b65de83b201a14217e0108313442ad44a13aca
kalysh_maxym <503292@ukr.net>
2020-06-12 16:54:19 +0300

    LOCKING bug

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import { getPosts } from '../services/api';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-const Container = styled.div`
-  font-family: sans-serif;
-  text-align: center;
-  color: grey;
-`;
+// const Container = styled.div`
+//   font-family: sans-serif;
+//   text-align: center;
+//   color: grey;
+// `;
 
 const App = () => {
   const [posts, setPost] = useState([]);
 
-  return <Container>Apppp</Container>;
+  return <>Apppp</>;
 };
 
 export default App;
